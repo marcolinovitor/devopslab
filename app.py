47187b54a2f3996e314c398560b2f3199f487c73
Vitor Marcolino <marcolino.vitor@gmail.com>
2022-02-09 20:24:27 -0300

    Incluindo Dockerfile

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def pagina_inicial():
     return "Hello World from DevOps & Cloud Lab! - Vitor Marcolino"
 
 if __name__ == '__main__':
-    app.run()
+    port = os.getenv('PORT')
+    app.run('0.0.0.0', port = port)
